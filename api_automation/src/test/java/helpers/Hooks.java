0c780d64f7b7c57fd22402886d4bb78942588267
Mareks <56604457+Mareks04@users.noreply.github.com>
2025-03-27 21:23:06 +0200

    print statement updated

--- api_automation/src/test/java/helpers/Hooks.java
+++ api_automation/src/test/java/helpers/Hooks.java
@@ -27,6 +27,6 @@ public class Hooks {
 
     @After
     public void tearDown(Scenario scenario) {
-        System.out.println("Scenario: '" + scenario.getName() + "' has ended");
+        System.out.println("Scenario: '" + scenario.getName() + "' has finished");
     }
 }
